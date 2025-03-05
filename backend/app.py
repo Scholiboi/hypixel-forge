5d23f8e8d1eaf2a2735df03ae14b110516ce0a98
Scholiboi <115933593+Scholiboi@users.noreply.github.com>
2025-03-05 15:47:24 +0530

    deployment branch

--- backend/app.py
+++ backend/app.py
@@ -7,21 +7,13 @@ from routes.resource_route import rr_bp
 from routes.sandbox_route import sr_bp
 from routes.inventoryresources_route import ir_bp
 
-if getattr(sys, '_MEIPASS', False):
-    base_path = sys._MEIPASS
-else:
-    base_path = os.path.abspath(os.path.dirname(__file__))
+base_path = sys._MEIPASS
 
-if getattr(sys, '_MEIPASS', False):
-    # this is for when frozen, the frontend build was bundled inside the executable
-    BUILD_DIR = os.path.join(base_path, 'frontend', 'build')
-    RECIPES_DIR = os.path.join(base_path, 'recipes')
-    INSTANCE_DIR = os.path.join(base_path, 'instance')
-else:
-    # otherwise during dev, use relative paths 
-    BUILD_DIR = os.path.join(os.path.dirname(__file__), '..', 'frontend', 'build')
-    RECIPES_DIR = os.path.join(os.path.dirname(__file__), 'recipes')
-    INSTANCE_DIR = os.path.join(os.path.dirname(__file__), 'instance')
+# this is for when frozen, the frontend build was bundled inside the executable
+BUILD_DIR = os.path.join(base_path, 'frontend', 'build')
+RECIPES_DIR = os.path.join(base_path, 'recipes')
+INSTANCE_DIR = os.path.join(base_path, 'instance')
+
 
 app = Flask(__name__, static_folder=BUILD_DIR)
 CORS(app)
--- backend/routes/sandbox_route.py
+++ backend/routes/sandbox_route.py
@@ -7,11 +7,7 @@ from models import Resource, db
 sr_bp = Blueprint(name='sandbox_route', import_name=__name__, url_prefix='/api/sandbox')
 
 def resource_path(relative_path):
-    # Get absolute path to resource, works for dev and for PyInstaller.!!!!!!
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    base_path = sys._MEIPASS
     return os.path.join(base_path, relative_path)
 
 @sr_bp.route('/get-recipes-names', methods=["GET"])
